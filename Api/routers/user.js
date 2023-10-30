430d6788aaaa8507a56e000523ad876b7b130ea3
olliebalabil <oliverbalabil@gmail.com>
2023-10-30 12:12:24 +0000

    added updates for subject xps

--- Api/routers/user.js
+++ Api/routers/user.js
@@ -12,5 +12,8 @@ userRouter.get("/english", userController.indexEnglish)
 userRouter.get("/science", userController.indexScience)
 userRouter.get("/:id", userController.show)
 userRouter.patch("/:id", userController.update)
+userRouter.patch("/maths/:id", userController.updateMaths)
+userRouter.patch("/english/:id", userController.updateEnglish)
+userRouter.patch("/science/:id", userController.updateScience)
 
 module.exports = userRouter;
