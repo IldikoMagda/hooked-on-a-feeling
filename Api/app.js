686ca48bd9c0f709af7a799afc483385345d2671
Abdulrahman <abdulx9999@gmail.com>
2023-10-30 09:34:05 +0000

    Created Index,App .js

---
+++ Api/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const logger = require('morgan')
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(logger('dev'))
+
+app.get("/", (req, res) => {
+  res.json({
+        title: "Crammer Education",
+        description: "The project is designed to encapsulate everything that has been covered so far on the course."
+    })
+});
+
+module.exports = app;
