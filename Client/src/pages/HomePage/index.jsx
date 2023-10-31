bfb74b28f37caec45ba2415a52b789e406d85fee
stephenmurray3 <stephenmurray@live.com>
2023-10-31 10:13:39 +0000

    Added homepage fetch request

--- Client/src/pages/HomePage/index.jsx
+++ Client/src/pages/HomePage/index.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom';
 import { Home } from '../../components'
 
 export default function HomePage() {
+
+  const [user, setUser] = useState();
+  const [tasks, setTasks] = useState([]);
+  const { id } = useParams();
+
+  async function displayUser(id) {
+    const response = await fetch(`https://project-3-backend-l4m5.onrender.com/users/${id}`);
+    const data = await response.json();
+    setUser(data);
+    // console.log(data);
+
+  }
+
   return (
-    <Home/>
+    <>
+      <Home />
+    </>
   )
 }
