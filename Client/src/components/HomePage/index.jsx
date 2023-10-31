4cbab2f0771ef11f7f6c1ee3314303e9d8498824
James Threadgill <jpgthreadgill97@gmail.com>
2023-10-31 14:41:23 +0000

    Added sprite switch case

--- Client/src/components/HomePage/index.jsx
+++ Client/src/components/HomePage/index.jsx
@@ -1,17 +1,43 @@
 import React from "react";
 import BasicSprite from "../../assets/BasicSprite.png";
+import BasicRed from "../../assets/Red/BasicRed.png";
+import BasicGreen from "../../assets/Green/BasicGreen.png";
+import BasicBlue from "../../assets/Blue/BasicBlue.png";
+import BasicOrange from "../../assets/Orange/BasicOrange.png";
+
 import { useAuth } from "../../contexts";
 
 function HomePage() {
   const { userData } = useAuth();
   console.log(userData);
+  let userSprite = ""
+
+  const getSpritePath = (color) => {
+    switch (color) {
+      case 'orange':
+        return BasicOrange;
+      case 'green':
+        return BasicGreen;
+      case 'red':
+        return BasicRed;
+      case 'blue':
+        return BasicBlue;
+      default:
+        return BasicSprite; // Default path if color is not matched
+    }
+  };
+
+  // Get the sprite path
+  const spritePath = getSpritePath(userData.favcolor);
+  console.log(spritePath);
 
   return (
     <div className="home-container">
       <div className="left-box">
         <div className="profile-card">
           <div className="profile-sprite">
-            <img src={BasicSprite} alt="User Sprite" className="sprite" />
+            <img src={spritePath} alt="User Sprite" className="sprite" />
+            
           </div>
           <div className="profile-header">
             <h2>{userData.username}</h2>
