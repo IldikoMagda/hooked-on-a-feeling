9fdcb11b5092a685c1815f3932d31139fd708c70
Abdulrahman <abdulx9999@gmail.com>
2023-10-31 09:55:34 +0000

    Added Authenticator Middleware

--- Api/database/setup.sql
+++ Api/database/setup.sql
@@ -50,7 +50,7 @@ VALUES
     ('Go over Science notes', 'Conduct research for the upcoming Biology project.', '2023-07-01', 'Science', 2, 19);
 
 
-INSERT INTO user_account (username, password, generalXp, subjectXpMaths)
-VALUES ('Ollie', 'Password1', 10, 20), 
-       ('Abdul', 'Password2', 20, 10);
+INSERT INTO user_account (username, password, generalXp, subjectXpMaths,subjectXpEnglish,subjectXpScience)
+VALUES ('Ollie', 'Password1', 10, 20,12,34), 
+       ('Abdul', 'Password2', 20, 10,25,21);
 
