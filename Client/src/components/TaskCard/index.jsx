4846f4aa08a10223bf200540b9f3f0f08c22e71f
Abdulrahman <abdulx9999@gmail.com>
2023-11-01 12:19:38 +0000

    Added Animation to Complete Task

--- Client/src/components/TaskCard/index.jsx
+++ Client/src/components/TaskCard/index.jsx
@@ -1,9 +1,15 @@
 import React, {useState} from "react"
+import Swal from 'sweetalert2'
 
 export default function TaskCard ({task}) {
   const [completed,setCompleted] = useState(false);
   const handleCheckbox = () => {
     setCompleted(!completed)
+    Swal.fire(
+      'Task Completed',
+      'Nice Work. Keep it Up',
+      'success'
+    )
   }
   return (
     <>
