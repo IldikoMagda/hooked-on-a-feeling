07141e01da10138a4e29e808393b1eb5cc8c5182
IldikoMagda <ildiko.magda@gmail.com>
2023-11-01 13:29:46 +0000

    fixed color fetching for sprites started filters

--- Client/src/components/LeaderboardCard/index.jsx
+++ Client/src/components/LeaderboardCard/index.jsx
@@ -7,8 +7,8 @@ export default function LeaderboardCard({ username, generalxp, favcolor, index }
     return (
         <>
             <div className="leaderboard-entry">
-                <span className="leaderboard-position">#{index + 1}</span>
-                <span className="leaderboard-sprite"><img className="leaderSprite" src={getSpritePath({favcolor}, {generalxp})}/></span>
+                <span className="leaderboard-position">#{index + 1}{favcolor}</span>
+                <span className="leaderboard-sprite"><img className="leaderSprite" src={getSpritePath(favcolor, generalxp)}/></span>
                 <span className="leaderboard-username">{username}</span>
                 <span>XP: {generalxp}</span>
             </div>
--- Client/src/pages/LeaderboardPage/index.jsx
+++ Client/src/pages/LeaderboardPage/index.jsx
@@ -4,14 +4,21 @@ import { LeaderboardCard } from '../../components';
 
 export default function LeaderboardPage() {
 
+  //we have maths english and science and general xp  
+
   const [users, setUsers] = useState([]);
+  const [generalOnly, setGeneralOnly] = useState(true);
+  const [mathsOnly, setMathsOnly] = useState(false);
+  const [scienceOnly, setScienceOnly] = useState(false);
+  const [englishOnly, setEnglishOnly] = useState(false);
+
 
   useEffect(() => {
     const loadLeaderboard = async () => {
       const response = await fetch(`https://project-3-backend-l4m5.onrender.com/users`);
       const data = await response.json();
       const sortedUsers = data.slice().sort((a, b) => b.generalxp - a.generalxp);
-      // console.log(users)
+      console.log(users)
       const topUsers = sortedUsers.slice(0, 10);
       // console.log(topUsers);
       setUsers(topUsers);
