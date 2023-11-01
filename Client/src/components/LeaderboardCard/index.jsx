09c17332d87cababa1b82eebfd72db8c38f2e984
IldikoMagda <ildiko.magda@gmail.com>
2023-11-01 15:20:55 +0000

    leaderboard filters working

--- Client/src/components/LeaderboardCard/index.jsx
+++ Client/src/components/LeaderboardCard/index.jsx
@@ -2,15 +2,15 @@ import React, {useState, useEffect} from "react";
 import {useAuth} from "../../contexts";
 import getSpritePath from "../Sprites";
 
-export default function LeaderboardCard({ username, generalxp, favcolor, index }) {
+export default function LeaderboardCard({ username, subjectXp, favcolor, index }) {
 
     return (
         <>
             <div className="leaderboard-entry">
-                <span className="leaderboard-position">#{index + 1}{favcolor}</span>
-                <span className="leaderboard-sprite"><img className="leaderSprite" src={getSpritePath(favcolor, generalxp)}/></span>
+                <span className="leaderboard-position">#{index + 1}</span>
+                <span className="leaderboard-sprite"><img className="leaderSprite" src={getSpritePath(favcolor, subjectXp)}/></span>
                 <span className="leaderboard-username">{username}</span>
-                <span>XP: {generalxp}</span>
+                <span>XP: {subjectXp}</span>
             </div>
         </>
     )
