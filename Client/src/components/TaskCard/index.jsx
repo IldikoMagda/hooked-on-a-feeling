9b3bcbed3f491ee4f666c8abbbb87d8873d735d9
James Threadgill <jpgthreadgill97@gmail.com>
2023-11-02 10:49:06 +0000

    Styling for task cards and a little bit of modal styling. Added scrollbar styling

--- Client/src/components/TaskCard/index.jsx
+++ Client/src/components/TaskCard/index.jsx
@@ -1,20 +1,14 @@
-import React, {useState} from "react"
-import Swal from 'sweetalert2'
-import Modal from '../Modal';
+import React, { useState } from "react";
+import Swal from "sweetalert2";
+import Modal from "../Modal";
 import EditForm from "../EditForm";
 
-
-export default function TaskCard ({task,completeTask}) {
-  
-  const [completed,setCompleted] = useState(false);
+export default function TaskCard({ task, completeTask }) {
+  const [completed, setCompleted] = useState(false);
   const handleCheckbox = () => {
-    setCompleted(!completed)
-    Swal.fire(
-      'Task Completed',
-      'Nice Work. Keep it Up',
-      'success'
-    )
-  }
+    setCompleted(!completed);
+    Swal.fire("Task Completed", "Nice Work. Keep it Up", "success");
+  };
 
   //for modal
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -28,26 +22,42 @@ export default function TaskCard ({task,completeTask}) {
   };
   return (
     <>
-
-        <div className="todo-item">
+      <div className="todo-item">
+        <div className="todo-title">
           <h3>{task.title}</h3>
-          <p>{task.content}</p>
-          <p>{task.subject}</p>
-          <p>Due: {task.duedate.substring(0,10)}</p>
-          <p>XP Worth: {task.generalxp}</p>
-          <input type="checkbox" onChange={handleCheckbox}  onClick={() => completeTask(task.item_id)}/> Completed
-          <div>
-            <button className="rpg-button" onClick={openModal}>Edit Task</button>
-            <Modal isOpen={isModalOpen} >
-
-              <h2>Edit Task</h2>
-              <div className="create-task-modal">
-                <EditForm task={task} closeModal={closeModal}/>
-              </div>
-              
-            </Modal>
-          </div>
         </div>
+        <div className="todo-content">
+          <p>{task.content}</p>
+        </div>
+        <div className="todo-subject">
+          <p>{task.subject}</p>
+        </div>
+        <div className="todo-duedate">
+          <p>Due: {task.duedate.substring(0, 10)}</p>
+        </div>
+        <div className="todo-xp">
+          <p>XP Worth: {task.generalxp}</p>
+        </div>
+        <div className="todo-completed">
+          <input
+            type="checkbox"
+            onChange={handleCheckbox}
+            onClick={() => completeTask(task.item_id)}
+          />{" "}
+          Completed
+        </div>
+        <div>
+          <button className="rpg-button todo-edit-button" onClick={openModal}>
+            Edit Task
+          </button>
+          <Modal isOpen={isModalOpen}>
+            <h2>Edit Task</h2>
+            <div className="create-task-modal">
+              <EditForm task={task} closeModal={closeModal} />
+            </div>
+          </Modal>
+        </div>
+      </div>
     </>
-  )
+  );
 }
